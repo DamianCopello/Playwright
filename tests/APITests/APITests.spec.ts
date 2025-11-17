43629c02971c0cec269046359ab2ccdf663a5188
DamianCopello <copello.dam@gmail.com>
2025-11-17 19:51:09 -0300

    Restore project files

--- tests/APITests/APITests.spec.ts
+++ tests/APITests/APITests.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-const REPO = 'Playwright';
+const REPO = 'APITesting';
 const USER = 'DamianCopello';
 
 test('Validate I can create an Issue in this repo', async ({ request }) => {
@@ -12,4 +12,33 @@ test('Validate I can create an Issue in this repo', async ({ request }) => {
         },
     });
     expect(newIssueResponse.status()).toBe(201);
+
+ const issues = await request.get(`/repos/${USER}/${REPO}/issues`);
+    expect(issues.ok()).toBeTruthy();
+    expect(await issues.json()).toContainEqual(expect.objectContaining({
+        title: '[BUG] Issue created via Playwright API',
+        body: 'This is a test issue created using Playwright API testing capabilities.'
+     }));
+});
+
+test('Validate I can create a Feature in this repo', async ({ request }) => {
+    const newIssue = await request.post(`/repos/${USER}/${REPO}/issues`, {
+        data: {
+            title: '[Feature] Issue created via Playwright API',
+            body: 'This is a test issue created using Playwright API testing capabilities.'
+        }
+    });
+    expect(newIssue.ok()).toBeTruthy();
+ 
+    const issues = await request.get(`/repos/${USER}/${REPO}/issues`);
+    expect(issues.ok()).toBeTruthy();
+    expect(await issues.json()).toContainEqual(expect.objectContaining({
+        title: '[Feature] Issue created via Playwright API',
+        body: 'This is a test issue created using Playwright API testing capabilities.'
+    }));
+});
+ 
+test.afterAll(async ({ request }) => {
+    const response = await request.delete(`/repos/${USER}/${REPO}`);
+    expect(response.ok()).toBeTruthy();
 });
